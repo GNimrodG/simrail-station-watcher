f5d05889d5e3810aefa8124be25b741ff7152b11
Nimród Glöckl <g.nimrod.g@gmail.com>
2023-11-22 11:48:51 +0100

    Added new station EDR mappings & sorted list

--- src/edr-mapping.ts
+++ src/edr-mapping.ts
@@ -2,17 +2,20 @@ import { Station } from "./types";
 
 export const edrMap = new Map<string, string>([
   ["B", "T1_BZ"],     // Będzin
-  ["Gr", "GRO_MAZ"],  // Grodzisk Mazowiecki
-  ["Id", "IDZ"],      // Idzikowice
-  ["Op", "OP_PO"],    // Opoczno Południe
-  ["Zw", "ZA"],       // Zawiercie
   ["Dra", "DOR"],     // Dorota
   ["DW", "DG_WZ"],    // Dąbrowa Górnicza Wschodnia
+  ["DZ", "DG_ZA"],    // Dąbrowa Górnicza Ząbkowice
+  ["Gr", "GRO_MAZ"],  // Grodzisk Mazowiecki
+  ["Id", "IDZ"],      // Idzikowice
   ["Kr", "KOR"],      // Korytów
+  ["ŁA", "LZ_LA"],    // Łazy Ła
   ["ŁC", "LZ_LC"],    // Łazy Łc
   ["Ol", "OZ"],       // Olszamowice
+  ["Op", "OP_PO"],    // Opoczno Południe
   ["Se", "SZE"],      // Szeligi
+  ["Sl", "SLK"],      // Sławków
   ["Spł1", "SG_PO"],  // Sosnowiec Południowy
+  ["Zw", "ZA"],       // Zawiercie,
 ]);
 
 export function getStationEDRLink(station: Station, serverCode: string) {
