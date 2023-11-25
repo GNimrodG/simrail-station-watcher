da402021a9d5c52208db0b2182d25583ba8ab9a1
Nimród Glöckl <g.nimrod.g@data-unknown.eu>
2023-11-25 13:58:00 +0100

    Added snetry vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "data-unknown",
+    project: "simrail-station-watcher"
+  })],
   build: {
     rollupOptions: {
       output: {
@@ -11,6 +15,8 @@ export default defineConfig({
           "mui-icons": ["@mui/icons-material"],
         }
       }
-    }
+    },
+
+    sourcemap: true
   }
 });
